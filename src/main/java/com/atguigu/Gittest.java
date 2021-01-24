53932c838f12bd4eabad1c4fb6f675d7f71191cc
HLCadc <1076046840@qq.com>
2021-01-24 19:05:57 +0800

    更新二

--- src/main/java/com/atguigu/Gittest.java
+++ src/main/java/com/atguigu/Gittest.java
@@ -3,6 +3,6 @@ package com.atguigu;
 public class Gittest {
     public static void main(String[] args) {
         System.out.println("11111111");
-        System.out.println("222222222");
+
     }
 }
