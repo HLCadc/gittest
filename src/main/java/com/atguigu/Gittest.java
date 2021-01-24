dac21a9f77ff8d4a7848b977b9e85bc5fbe6620e
HLCadc <1076046840@qq.com>
2021-01-24 19:12:50 +0800

    xxxxxxxxxx

--- src/main/java/com/atguigu/Gittest.java
+++ src/main/java/com/atguigu/Gittest.java
@@ -4,5 +4,6 @@ public class Gittest {
     public static void main(String[] args) {
         System.out.println("11111111");
         System.out.println("222222222");
+        System.out.println("xxxxxxxx");
     }
 }
